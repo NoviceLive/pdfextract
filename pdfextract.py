0d1526c8c06aa8f546f1a0be56f4ae69c0c1ec94
NoviceLive <rectigu@gmail.com>
2015-04-16 16:51:18 +0800

    minor trivial updates

--- pdfextract.py
+++ pdfextract.py
@@ -7,7 +7,8 @@ by Novice Live, http://novicelive.org/ :)
 
 1. added support for multiple source documents.
 
-now you can merge the extracted pages in the very same command, no matter where these pages are extracted from.
+now you can merge the extracted pages in the very same command,
+no matter where these pages are extracted from.
 
 feb 7, 2015
 
@@ -36,6 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import re
 import argparse
 import sys
+import traceback
 
 import PyPDF2
 
@@ -44,7 +46,7 @@ def main():
         description='DESCRIPTION\n\n\tmanipulate pages either in the same pdf or in different pdf documents\n\n\textract, remove, repeat or reorder, and if you like, after all of these manipulations, you can then merge them\n\n\tpage numbers start from 1\n\n\tsupport multiple source documents',
         epilog='FULL PAGE RANGE SYNTAXES\n\n\t1. [start][-][end] specifies a single page range. if <start> is not present or is 0 or is greater than <end> (this <end> is that <end> which will not be greater than the page count), then <start> will be 1; if <end> is absent or 0 or greater than the page count, then <end> will be the page count. you can not leave both <start> and <end> unspecified unless the - is also absent. if there is only a number, then both <start> and <end> will be determined from the number.\n\n\t2. + is used to combine several single page ranges. specially, merely applying a single + means 0-0+0-0. thus only using + will double the pdf, ++ will triple it, and so on.\n\n\t3. under these two syntaxes, freedom is granted.\n\nEXAMPLE USES\n\n\t1. pdfextract test.pdf:5-\n\textracting pages from the 5th to the last (suppose it is no less than 5 pages)\n\n\t2. pdfextract test.pdf:-5\n\textracting from the first page to the 5th (suppose it is no less than 5 pages)\n\n\t3. pdfextract test.pdf:-5+7- -o no-6.pdf\n\tremoving the 6th page, naming the new pdf as no-6.pdf (suppose it has more than 6 pages)\n\n\t4. pdfextract test.pdf:++\n\ttripling the source pdf, with the same pages repeated two more times\n\n\t5. pdfextract test.pdf:1+3+5+7+9\n\textracting the 1st, 3rd, 5th, 7th and 9th page, suppose it is no less than 9 pages',
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    
+
     parser.add_argument('pdf_colon_ops',
                         metavar='pdf_document:page_ranges',
                         nargs='+',
@@ -55,9 +57,7 @@ def main():
                         help='do not use the default output name (which is output.pdf). export the resulting document with the specified file name')
     parser.add_argument('-v', '--verbose',
                         dest='verbose',
-                        action='store_const',
-                        const=True,
-                        default=False,
+                        action='store_true',
                         help='display more error and warning information, as well as what is going on')
 
     args = parser.parse_args()
@@ -66,7 +66,7 @@ def main():
         args.output = 'output.pdf'
 
     writer = PyPDF2.PdfFileWriter()
-    
+
     for i in args.pdf_colon_ops:
         pdf_ops_list = i.split(':')
 
@@ -77,16 +77,19 @@ def main():
             pages = ''
 
         try:
-            reader = PyPDF2.PdfFileReader(pdf, warndest=None if args.verbose else sys.stdin)
+            reader = PyPDF2.PdfFileReader(
+                pdf,
+                warndest=None if args.verbose else sys.stdin
+            )
         except:
             print('could not read: {}'.format(pdf))
             if args.verbose:
-                print(get_cur_error())
-            exit() # you can just return, since you are in the main function
+                traceback.print_exc()
+            exit()
 
         if pages == '':
             pages = '1-' + str(reader.numPages)
-                    
+
         page_ranges = parse_page_ranges(pages)
 
         if page_ranges:
@@ -95,20 +98,26 @@ def main():
                     [
                         writer.addPage(reader.getPage(i))
                         ==
-                        (print('adding page {} from {}'.format(i + 1, pdf)) if args.verbose else None)
-                        
+                        (
+                            print('adding page {} from {}'.format(i + 1, pdf))
+                            if args.verbose else None
+                        )
+
                         for i in make_single_range(j, reader.numPages)
                     ]
                     for j in page_ranges
                 ]
             except:
-                print('add page error: {}'.format(get_cur_error()))
+                print('add page error')
+                if args.verbose:
+                    traceback.print_exc()
         else:
             print('no valid range in the specified ranges: {}'.format(pages))
 
     total_page_count = writer.getNumPages()
 
-    sys.setrecursionlimit(4 * sys.getrecursionlimit()) # let's take some risk
+    # let's take some risk
+    sys.setrecursionlimit(4 * sys.getrecursionlimit())
 
     if total_page_count:
         try:
@@ -117,56 +126,76 @@ def main():
         except:
             print('could not write: {}'.format(args.output))
             if args.verbose:
-                print(get_cur_error())
+                traceback.print_exc()
             exit()
-                
+
         print('exported {} page(s) successfully'.format(total_page_count))
     else:
         print('no valid page in the specified operations: {}'.format(pages))
 
 def parse_page_ranges(raw_range_string):
     """
-    parse the commandline string, which specifies how to behave, into a usable form
+    parse the commandline string,
+    which specifies how to behave, into a usable form
 
-    + param str raw_range_string: the raw string containing information about page rangs
+    + param str raw_range_string:
+        the raw string containing information about page rangs
 
     + return: all valid page ranges, each of them are in the form (start, end)
     + rtype: list
     """
-    return [parse_single_range(i) for i in raw_range_string.split('+') if check_single_range(i)]
+    return [parse_single_range(i)
+            for i in raw_range_string.split('+') if check_single_range(i)]
 
 def make_single_range(page_range, page_count):
     """
-    correct a single range, i.e., convert 1-indexed-from to 0-indexed-from and replace illegal vaule to conventionally defined value
+    correct a single range, i.e.,
+    convert 1-indexed-from to 0-indexed-from
+    and replace illegal vaule to conventionally defined value
 
-    + param tuple page_range: in the form (start, end), both <start> and <end> are 1-indexed-from.
-    + param int page_count: the page count of the source pdf, used to correct out-of-bound invalid upper range
+    + param tuple page_range:
+        in the form (start, end),
+        both <start> and <end> are 1-indexed-from.
 
-    + return: a range instance, producing values exactly the same in the input range, except here are 0-indexed-from
+    + param int page_count:
+        the page count of the source pdf,
+        used to correct out-of-bound invalid upper range
+
+    + return: a range instance,
+        producing values exactly the same in the input range,
+        except here are 0-indexed-from
+
     + rtype: range
     """
-    start, end = page_range # here are indexed from 1
-    end = end if end and end <= page_count else page_count # <end> do not need to subtract 1 because range() itself will not produce to its upper bound
-    start = start - 1 if start and start <= end else 0 # <start> has to be decremented by 1
-    return range(start, end) # directly return a range instance rather than a tuple
+    # here are indexed from 1
+    start, end = page_range
+    # <end> do not need to subtract 1
+    # because range() itself will not produce to its upper bound
+    end = end if end and end <= page_count else page_count
+    #  # <start> has to be decremented by 1
+    start = start - 1 if start and start <= end else 0
+    # directly return a range instance rather than a tuple
+    return range(start, end)
 
 def parse_single_range(single_range_string):
     """
-    parse separated single range strings into a tuple, in the form of (start, end)
+    parse separated single range strings into a tuple.
+    in the form of (start, end)
 
-    + param str single_range_string: a string of the form [start][-][end], which specifies a range
+    + param str single_range_string:
+        a string of the form [start][-][end], which specifies a range
 
     + return: (start, end)
     + rtype: tuple
     """
     if not single_range_string:
         return 1, 0
-    
+
     if single_range_string.endswith('-'):
         single_range_string += str(0)
     if single_range_string.startswith('-'):
         single_range_string = str(1) + single_range_string
-        
+
     single_range = single_range_string.split('-')
     start = int(single_range[0])
     end = int(single_range[1]) if len(single_range) == 2 else start
@@ -176,29 +205,23 @@ def check_single_range(single_range_string):
     """
     check the validity of a single range string before it can be parsed
 
-    + param str single_range_string: a string of the form [start][-][end], which specifies a range
+    + param str single_range_string:
+        a string of the form [start][-][end], which specifies a range
 
     + return true if valid else false
     + rtype: boolean
     """
     if single_range_string:
-        # re.fullmatch became available since python 3.4. compatibility troubles.
-        # if re.fullmatch(r'\d+\-|\-\d+|\d+\-\d+', single_range_string): 
-        if not re.sub(r'\d+\-\d+|\d+\-|\-\d+|\d+', '', single_range_string, 1): # move \d+\-\d to the first or this will not be right
+        # re.fullmatch became available since python 3.4.
+        # compatibility troubles.
+        # if re.fullmatch(r'\d+\-|\-\d+|\d+\-\d+', single_range_string):
+        # move \d+\-\d to the first or this will not be right
+        if not re.sub(r'\d+\-\d+|\d+\-|\-\d+|\d+', '', single_range_string, 1):
             return True
         else:
             print('syntax error: {}'.format(single_range_string))
             return False
     return True
-
-def get_cur_error():
-    """
-    get current error information and return the prompt string
-
-    + return: the error prompt string. None if no error
-    + rtype: str or boolean
-    """
-    return sys.exc_info()[1]
 
 if __name__ == '__main__':
     main()
