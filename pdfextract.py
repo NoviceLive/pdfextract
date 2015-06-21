74f9d9eb38032969414cf86a014f95b455d0bd04
NoviceLive <rectigu@gmail.com>
2015-06-21 22:15:43 +0800

    refined and added -l option to control the recursion limit

--- pdfextract.py
+++ pdfextract.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 
 """
@@ -56,7 +57,7 @@ def main(args):
         if pages == '':
             pages = '1-' + str(reader.numPages)
 
-        page_ranges = parse_page_ranges(pages)
+        page_ranges = parse_ranges(pages)
         if page_ranges:
             try:
                 [
@@ -68,7 +69,7 @@ def main(args):
                             if args.verbose else None
                         )
 
-                        for i in make_single_range(j, reader.numPages)
+                        for i in make_range(j, reader.numPages)
                     ]
                     for j in page_ranges
                 ]
@@ -81,8 +82,7 @@ def main(args):
 
     total_page_count = writer.getNumPages()
 
-    # let's take some risk
-    sys.setrecursionlimit(4 * sys.getrecursionlimit())
+    sys.setrecursionlimit(args.limit * sys.getrecursionlimit())
 
     if total_page_count:
         try:
@@ -99,146 +99,110 @@ def main(args):
         print('no valid page in the specified operations: {}'.format(pages))
 
 
-def parse_page_ranges(raw_range_string):
-    """
-    parse the commandline string,
-    which specifies how to behave, into a usable form
-
-    + param str raw_range_string:
-        the raw string containing information about page rangs
-
-    + return: all valid page ranges, each of them are in the form (start, end)
-    + rtype: list
-    """
-    return [parse_single_range(i)
-            for i in raw_range_string.split('+') if check_single_range(i)]
+def parse_ranges(ranges_string):
+    return [
+        parse_range(i)
+            for i in ranges_string.split('+') if check_range(i)
+    ]
 
 
-def make_single_range(page_range, page_count):
-    """
-    correct a single range, i.e.,
-    convert 1-indexed-from to 0-indexed-from
-    and replace illegal vaule to conventionally defined value
-
-    + param tuple page_range:
-        in the form (start, end),
-        both <start> and <end> are 1-indexed-from.
-
-    + param int page_count:
-        the page count of the source pdf,
-        used to correct out-of-bound invalid upper range
-
-    + return: a range instance,
-        producing values exactly the same in the input range,
-        except here are 0-indexed-from
-
-    + rtype: range
-    """
-    # here are indexed from 1
+def make_range(page_range, page_count):
     start, end = page_range
-    # <end> do not need to subtract 1
-    # because range() itself will not produce to its upper bound
     end = end if end and end <= page_count else page_count
-    #  # <start> has to be decremented by 1
     start = start - 1 if start and start <= end else 0
-    # directly return a range instance rather than a tuple
+
     return range(start, end)
 
 
-def parse_single_range(single_range_string):
-    """
-    parse separated single range strings into a tuple.
-    in the form of (start, end)
+def parse_range(range_string):
+    if range_string == '':
 
-    + param str single_range_string:
-        a string of the form [start][-][end], which specifies a range
-
-    + return: (start, end)
-    + rtype: tuple
-    """
-    if not single_range_string:
         return 1, 0
 
-    if single_range_string.endswith('-'):
-        single_range_string += str(0)
-    if single_range_string.startswith('-'):
-        single_range_string = str(1) + single_range_string
+    if range_string.endswith('-'):
+        range_string += str(0)
+    if range_string.startswith('-'):
+        range_string = str(1) + range_string
 
-    single_range = single_range_string.split('-')
+    single_range = range_string.split('-')
     start = int(single_range[0])
     end = int(single_range[1]) if len(single_range) == 2 else start
+
     return start, end
 
 
-def check_single_range(single_range_string):
-    """
-    check the validity of a single range string before it can be parsed
+def check_range(range_string):
+    if range_string == '':
 
-    + param str single_range_string:
-        a string of the form [start][-][end], which specifies a range
+        return True
 
-    + return true if valid else false
-    + rtype: boolean
-    """
-    if single_range_string:
-        # re.fullmatch became available since python 3.4.
-        # compatibility troubles.
-        # if re.fullmatch(r'\d+\-|\-\d+|\d+\-\d+', single_range_string):
-        # move \d+\-\d to the first or this will not be right
-        if not re.sub(r'\d+\-\d+|\d+\-|\-\d+|\d+', '', single_range_string, 1):
-            return True
-        else:
-            print('syntax error: {}'.format(single_range_string))
-            return False
-    return True
+    if re.sub(r'\d+\-\d+|\d+\-|\-\d+|\d+', '', range_string, 1) == '':
+
+        return True
+
+    print('syntax error: {}'.format(range_string))
+
+    return False
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="""DESCRIPTION
-\tmanipulate pages either in the same pdf or in different pdf documents
-\textract, remove, repeat or reorder, and if you like,
-\tafter all of these manipulations, you can then merge them
-\tpage numbers start from 1\n\n\tsupport multiple source documents""",
+        description=description,
         epilog=epilog,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
 
-    parser.add_argument('pdf_colon_ops', metavar='pdf_document:page_ranges',
-                        nargs='+',
-                        help="""the source document to operate on
-and the page ranges (full syntaxes see below). only specifying the pdf file
-means the whole document is targeted, i.e. 0-0 (see below :))""")
-
-    parser.add_argument('-o', '--output', metavar='pdf_output', dest='output',
-                        help="""do not use the default output name
-(which is output.pdf).
-export the resulting document with the specified file name""")
-
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
-                        help="""display more error and warning information,
-as well as what is going on""")
+    parser.add_argument(
+        'pdf_colon_ops',
+        metavar='PDF:ranges',
+        nargs='+',
+        help="the source PDF and ranges"
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        help="use the provided file name instead of output.pdf"
+    )
+    parser.add_argument(
+        '-l',
+        '--limit',
+        default=3,
+        type=int,
+        help="increase recursion limit the provided times"
+    )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help="turn on verbose mode"
+    )
 
     return parser.parse_args()
 
 
-epilog = """
-FULL PAGE RANGE SYNTAX
-\t1. [start][-][end] specifies a single page range.
+description = """
+DESCRIPTION
+\tmanipulate pages either in the same PDF or in different PDF documents.
+\textract, remove, repeat or reorder, and if you like,
+\tafter all of these manipulations, you can then merge them.
+\n\tpage numbers start from 1.\n\tmultiple source documents is supported.
+"""
 
-\t\tif <start> is not present or is 0 or is greater than
+
+epilog = """
+FULL SYNTAX
+\t1. [start][-][end] specifies a range.
+\n\t\tif <start> is not present or is 0 or is greater than
 \t\t<end> (this <end> is that <end> which won't be greater than the page count
 \t\t), then <start> will be 1; if <end> is absent or 0 or greater than
 \t\t the page count, then <end> will be the page count.
 \t\tyou can not leave both <start> and <end> unspecified
 \t\tunless the - is also absent. if there is only a number,
 \t\t then both <start> and <end> will be determined from the number.
-
-\t2. + is used to combine several single page ranges.
-
-\t\tspecially, merely applying a single + means 0-0+0-0.
+\n\t2. + is used to combine several ranges in the same source PDF.
+\n\t\tspecially, merely applying a single + means 0-0+0-0.
 \t\tthus only using + will double the pdf, ++ will triple it, and so on.
-
-\t3. under these two syntaxes, freedom is granted.
+\n\t3. under these two syntax, full freedom is granted.
 """
 
 
